7fcf58042dca7b320a6781be512bbdced88f40d5
PhatTurtle <phatrua2911@gmail.com>
2025-03-19 13:29:45 +0700

    UPDATE - PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
@@ -11,4 +18,14 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
+
+export default nextConfigFunction;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import "./globals.css";
 import { Outfit } from "next/font/google";
 import { HeaderComponent } from "@/components/HeaderComponents/HeaderComponent";
 import { FooterComponent } from "@/components/FooterComponents/FooterComponent";
-import { ThemeProvider } from "@/provider/ThemeProvider";
-import { Template } from "@/components/motion/template";
+import Head from "next/head";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -15,6 +14,13 @@ export const metadata: Metadata = {
   title: "Edward | Portfolio",
   description: "Introduction of Edward",
 
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Edward | Portfolio",
+    // startUpImage: [],
+  },
   icons: {
     icon: "/logo.svg",
     shortcut: "/logo.svg",
@@ -40,20 +46,67 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>My awesome PWA app</title>
+        <meta name="description" content="Best PWA app in the world!" />
+        <link rel="shortcut icon" href="/logo.png" />
+        <link rel="mask-icon" href="/logo.png" color="#FFFFFF" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/logo.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="My awesome PWA app" />
+        <meta name="twitter:description" content="Best PWA app in the world!" />
+        <meta name="twitter:image" content="/icons/twitter.png" />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="My awesome PWA app" />
+        <meta property="og:description" content="Best PWA app in the world!" />
+        <meta property="og:site_name" content="My awesome PWA app" />
+        <meta property="og:url" content="https://yourdomain.com" />
+        <meta property="og:image" content="/icons/og.png" />
+        {/* add the following only if you want to add a startup image for Apple devices. */}
         <link
-          rel="apple-touch-icon"
-          href="https://i.imgur.com/eQeFxx5.png"
-          type="image/png"
-          sizes="24"
+          rel="apple-touch-startup-image"
+          href="/logo.png"
+          sizes="2048x2732"
         />
         <link
-          rel="icon"
-          href="https://i.imgur.com/eQeFxx5.png"
-          type="image/png"
-          sizes="24"
+          rel="apple-touch-startup-image"
+          href="/logo.png"
+          sizes="1668x2224"
         />
-      </head>
+        <link
+          rel="apple-touch-startup-image"
+          href="/logo.png"
+          sizes="1536x2048"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/logo.png"
+          sizes="1125x2436"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/logo.png"
+          sizes="1242x2208"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/logo.png"
+          sizes="750x1334"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/logo.png"
+          sizes="640x1136"
+        />
+      </Head>
       <body className={`${outfit.className} antialiased`}>
         {/* <ThemeProvider
           attribute="class"
